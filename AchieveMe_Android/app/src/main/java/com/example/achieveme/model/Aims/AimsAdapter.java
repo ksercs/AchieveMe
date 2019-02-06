4ecd7986a777ffe1663351f0e2c8c606ec0efd3b
Maksim Timokhin <maximtim@outlook.com>
2019-02-07 01:18:05 +0300

    kaif

--- AchieveMe_Android/app/src/main/java/com/example/achieveme/model/Aims/AimsAdapter.java
+++ AchieveMe_Android/app/src/main/java/com/example/achieveme/model/Aims/AimsAdapter.java
@@ -1,13 +1,15 @@
-package com.example.achieveme.model.Lists;
+package com.example.achieveme.model.Aims;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
-import com.example.achieveme.model.Aims.AimRes;
+import com.example.achieveme.R;
 
 import java.util.List;
 
@@ -17,7 +19,7 @@ public class AimsAdapter extends ArrayAdapter {
     private List<AimRes> values;
 
     public AimsAdapter(Context context, List<AimRes> values) {
-        super(context, android.R.layout.simple_list_item_1, values);
+        super(context, R.layout.aim_list_item, values);
 
         this.context = context;
         this.values = values;
@@ -25,21 +27,22 @@ public class AimsAdapter extends ArrayAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        View row = convertView;
 
+        View row = convertView;
         if (row == null) {
             LayoutInflater inflater =
                     (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            row = inflater.inflate(android.R.layout.simple_list_item_1, parent, false);
+            row = inflater.inflate(R.layout.aim_list_item, parent, false);
         }
-
-        TextView textView = row.findViewById(android.R.id.text1);
+        CheckBox completed = row.findViewById(R.id.isCompleted);
+        final TextView textView = row.findViewById(R.id.aimNameView);
 
         AimRes item = values.get(position);
 
-        String message = item.getFields().getName();
-        textView.setText(message);
+        completed.setChecked(item.getFields().isIs_made());
+        textView.setText(item.getFields().getName());
         textView.setTag(item.getId());
+
         return row;
     }
 }
