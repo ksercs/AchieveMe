969990be783aa0e4f03bca7b51ae07dc5a2e581d
Maksim Timokhin <maximtim@outlook.com>
2019-02-08 14:04:42 +0300

    port 3636

---
+++ AchieveMe_Android/app/src/main/java/com/example/achieveme/model/Aims/newAimInfo.java
@@ -0,0 +1,5 @@
+package com.example.achieveme.model.Aims;
+
+public class newAimInfo {
+
+}
--- AchieveMe_Android/app/src/main/java/com/example/achieveme/remote/AimService.java
+++ AchieveMe_Android/app/src/main/java/com/example/achieveme/remote/AimService.java
@@ -3,10 +3,13 @@ package com.example.achieveme.remote;
 
 import com.example.achieveme.model.Aims.AimRes;
 import com.example.achieveme.model.Aims.SubAimRes;
+import com.example.achieveme.model.Aims.newAimInfo;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
+import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 public interface AimService {
