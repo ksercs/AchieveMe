315d7190205d63a54a97427b041aaeabfbe89c97
vadim808 <vch.98@mail.ru>
2019-02-09 16:48:50 +0300

    a

--- AchieveMe/media/collage.py
+++ AchieveMe/media/collage.py
@@ -1,11 +1,13 @@
 import subprocess
-import os  
-def fcollage():
-    tree = os.walk("/home/mtimokhin/AchieveMe/AchieveMe/media/images")
+import os 
+
+def fcollage(): 
+    tree = os.walk("/Users/mak/AchieveMe/AchieveMe/media/images")
     for i in tree:
-        image = i[2][1:]
-    pictures = "montage -geometry 500x200 "
+        image = i[2]
+    pictures = "montage -geometry 300x "  
     for i in image:
-        pictures += "/home/mtimokhin/AchieveMe/AchieveMe/media/images/" + i + " "
-    pictures += "/home/mtimokhin/AchieveMe/AchieveMe/media/collage.png"
+        pictures += "/Users/mak/AchieveMe/AchieveMe/media/images/" + i
+    pictures += " /Users/mak/AchieveMe/AchieveMe/media/collage.png"
+    print(pictures)
     subprocess.call(pictures, shell=True)
