183f207062b113c1899699cfa4e912233f65efa0
Vadim808 <vch.98@mail.ru>
2019-02-09 13:00:44 +0300

    Update collage.py
    fix

--- AchieveMe/media/collage.py
+++ AchieveMe/media/collage.py
@@ -1,11 +1,12 @@
 import subprocess
 import os  
 def fcollage():
-    tree = os.walk("~/AchieveMe/AchieveMe/media/images")
+    /home/mtimokhin/AchieveMe/AchieveMe/media/
+    tree = os.walk("/home/mtimokhin/AchieveMe/AchieveMe/media/images")
     for i in tree:
         image = i[2][1:]
     pictures = "montage "
     for i in image:
-        pictures += "~/AchieveMe/AchieveMe/media/images/" + i + " "
-    pictures += "~/AchieveMe/AchieveMe/media/collage.png"
+        pictures += "/home/mtimokhin/AchieveMe/AchieveMe/media/images/" + i + " "
+    pictures += "/home/mtimokhin/AchieveMe/AchieveMe/media/collage.png"
     subprocess.call(pictures, shell=True)
