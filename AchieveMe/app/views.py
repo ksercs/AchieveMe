b560d6719d56c2083a0d496abe5a46d997e70803
ksercs <ksercs0@gmail.com>
2019-02-09 17:56:04 +0300

    front for entities fixed

--- AchieveMe/app/views.py
+++ AchieveMe/app/views.py
@@ -353,6 +353,7 @@ def editListView(request, username, pk):
 def editAimView(request, username, listid, pk):
     lists = ListModel.objects.filter(user_name = username)
     aims = Aim.objects.filter(user_name = username, list_id = listid, parent_id = -1, is_completed = False)
+    completed = Aim.objects.filter(user_name = username, list_id = listid, parent_id = -1, is_completed = True)
     list = ListModel.objects.get(id = listid)
     cur_aim = Aim.objects.get(id = pk)
     vars = dict(
@@ -362,7 +363,8 @@ def editAimView(request, username, listid, pk):
         formA = AimForm(),
         formB = ListForm(),
         list_link = "/"+username+"/lists/",
-        ListAddingForm = ListForm()
+        ListAddingForm = ListForm(),
+        completed = completed
         )
 
     if request.method == 'POST' and 'aimbtn' in request.POST:
