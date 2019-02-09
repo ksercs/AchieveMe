fa763b6a57d73bb1751b705ca59ed70ee981972d
Vadim808 <vch.98@mail.ru>
2019-02-09 14:17:29 +0300

    Update collage.py

--- AchieveMe/media/collage.py
+++ AchieveMe/media/collage.py
@@ -4,7 +4,7 @@ def fcollage():
     tree = os.walk("/home/mtimokhin/AchieveMe/AchieveMe/media/images")
     for i in tree:
         image = i[2][1:]
-    pictures = "montage "
+    pictures = "montage -geometry 500x200 "
     for i in image:
         pictures += "/home/mtimokhin/AchieveMe/AchieveMe/media/images/" + i + " "
     pictures += "/home/mtimokhin/AchieveMe/AchieveMe/media/collage.png"
