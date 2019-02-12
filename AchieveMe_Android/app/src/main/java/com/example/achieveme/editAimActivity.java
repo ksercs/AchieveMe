82932db83e0b47b832ed3a17b4bf9b26aa0fff5a
Maksim Timokhin <maximtim@outlook.com>
2019-02-12 15:49:44 +0300

    zapret na invalid daty

--- AchieveMe_Android/app/src/main/java/com/example/achieveme/editAimActivity.java
+++ AchieveMe_Android/app/src/main/java/com/example/achieveme/editAimActivity.java
@@ -4,7 +4,10 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -18,8 +21,11 @@ import com.example.achieveme.remote.ApiUtils;
 import com.example.achieveme.remote.AsyncTaskLoadImage;
 
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -27,10 +33,10 @@ import retrofit2.Response;
 
 public class editAimActivity extends AppCompatActivity {
 
-    TextView name;
-    TextView date;
-    TextView time;
-    TextView description;
+    EditText name;
+    EditText date;
+    EditText time;
+    EditText description;
 
     SharedPreferences creds;
     String username;
@@ -53,13 +59,139 @@ public class editAimActivity extends AppCompatActivity {
         pos = intent.getIntExtra("pos", -1);
 
         name = findViewById(R.id.nameView);
-        date = findViewById(R.id.dateView);
+        date = (EditText) findViewById(R.id.dateView);
         time = findViewById(R.id.timeView);
         description = findViewById(R.id.descriptionView);
 
         creds = getSharedPreferences("creds", MODE_PRIVATE);
         username = creds.getString(LoginActivity.USERNAME, null);
         password = creds.getString(LoginActivity.PASSWORD, null);
+
+        TextWatcher dtw = new TextWatcher() {
+            private String current = "";
+            private String ddmmyyyy = "ГГГГММДД";
+            private Calendar cal = Calendar.getInstance();
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (!s.toString().equals(current)) {
+                    String clean = s.toString().replaceAll("[^\\d.]|\\.", "");
+                    String cleanC = current.replaceAll("[^\\d.]|\\.", "");
+
+                    int cl = clean.length();
+                    int sel = cl;
+                    if (cl >= 4) {
+                        ++sel;
+                    }
+                    if (cl >= 6) {
+                        ++sel;
+                    }
+
+                    //Fix for pressing delete next to a forward slash
+                    if (clean.equals(cleanC)) sel--;
+
+                    if (clean.length() < 8){
+                        clean = clean + ddmmyyyy.substring(clean.length());
+                    }else{
+                        //This part makes sure that when we finish entering numbers
+                        //the date is correct, fixing it otherwise
+                        int day  = Integer.parseInt(clean.substring(6,8));
+                        int mon  = Integer.parseInt(clean.substring(4,6));
+                        int year = Integer.parseInt(clean.substring(0,4));
+
+                        mon = mon < 1 ? 1 : mon > 12 ? 12 : mon;
+                        cal.set(Calendar.MONTH, mon-1);
+                        year = (year<1900)?1900:(year>2100)?2100:year;
+                        cal.set(Calendar.YEAR, year);
+                        // ^ first set year for the line below to work correctly
+                        //with leap years - otherwise, date e.g. 29/02/2012
+                        //would be automatically corrected to 28/02/2012
+                        day = day < 1 ? 1 : day;
+                        day = (day > cal.getActualMaximum(Calendar.DATE))? cal.getActualMaximum(Calendar.DATE):day;
+                        clean = String.format(Locale.US, "%02d%02d%02d",year, mon, day);
+                    }
+
+                    clean = String.format("%s-%s-%s", clean.substring(0, 4),
+                            clean.substring(4, 6),
+                            clean.substring(6, 8));
+
+                    sel = sel < 0 ? 0 : sel;
+                    current = clean;
+                    date.setText(current);
+                    date.setSelection(sel < current.length() ? sel : current.length());
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        };
+
+        TextWatcher ttw = new TextWatcher() {
+            private String current = "";
+            private String ddmmyyyy = "ЧЧММ";
+            private Calendar cal = Calendar.getInstance();
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (!s.toString().equals(current)) {
+                    String clean = s.toString().replaceAll("[^\\d.]|\\.", "");
+                    String cleanC = current.replaceAll("[^\\d.]|\\.", "");
+
+                    int cl = clean.length();
+                    int sel = cl;
+                    if (cl >= 2) {
+                        ++sel;
+                    }
+                    //Fix for pressing delete next to a forward slash
+                    if (clean.equals(cleanC)) sel--;
+
+                    if (clean.length() < 4){
+                        clean = clean + ddmmyyyy.substring(clean.length());
+                    }else{
+                        //This part makes sure that when we finish entering numbers
+                        //the date is correct, fixing it otherwise
+                        int hours = Integer.parseInt(clean.substring(0,2));
+                        int mins = Integer.parseInt(clean.substring(2,4));
+
+                        hours = hours < 0 ? 0 : hours > 23 ? 23 : hours;
+                        cal.set(Calendar.HOUR, hours);
+                        mins = mins < 0 ? 0 : mins > 60 ? 60 : mins;
+                        cal.set(Calendar.MINUTE, mins);
+                        // ^ first set year for the line below to work correctly
+                        //with leap years - otherwise, date e.g. 29/02/2012
+                        //would be automatically corrected to 28/02/2012
+                        clean = String.format(Locale.US, "%02d%02d",hours, mins);
+                    }
+
+                    clean = String.format("%s:%s", clean.substring(0, 2),
+                            clean.substring(2, 4));
+
+                    sel = sel < 0 ? 0 : sel;
+                    current = clean;
+                    time.setText(current);
+                    time.setSelection(sel < current.length() ? sel : current.length());
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        };
+
+
+        date.addTextChangedListener(dtw);
+        time.addTextChangedListener(ttw);
 
         if (pos >= 0) {
             AimService aimService = ApiUtils.getAimService();
@@ -99,7 +231,18 @@ public class editAimActivity extends AppCompatActivity {
         }
     }
 
+
     public void sendInfo(View view) {
+        Date now = new Date();
+        if (date.getText().toString().isEmpty() || date.getText().toString().equals("ГГГГ-ММ-ДД")) {
+            date.setText((new SimpleDateFormat("yyyy-MM-dd")).format(now));
+        }
+        if (time.getText().toString().isEmpty() || time.getText().toString().equals("ЧЧ:ММ")) {
+            time.setText((new SimpleDateFormat("HH:mm")).format(now));
+        }
+        if (name.getText().toString().isEmpty()) {
+            name.setText("Новая цель");
+        }
         Aim new_aim = new Aim(
                 name.getText().toString(),
                 date.getText().toString(),
--- AchieveMe_Android/app/src/main/java/com/example/achieveme/remote/ApiUtils.java
+++ AchieveMe_Android/app/src/main/java/com/example/achieveme/remote/ApiUtils.java
@@ -2,7 +2,7 @@ package com.example.achieveme.remote;
 
 public class ApiUtils {
 
-    public static final String BASE_URL = "http://192.168.43.208:3636/";
+    public static final String BASE_URL = "http://10.23.74.12:3636/";
 
     public static LoginService getLoginService () {
         return RetrofitClient.getClient(BASE_URL).create(LoginService.class);
