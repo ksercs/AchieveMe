acd7849397142359e8ec5f8ea074560b7aecdbb8
Anton Kuznetsov <37047972+ksercs@users.noreply.github.com>
2019-08-05 19:33:27 +0300

    Update settings.py

--- AchieveMe/AchieveMe/settings.py
+++ AchieveMe/AchieveMe/settings.py
@@ -34,8 +34,8 @@ DEFAULT_FROM_EMAIL = 'info@google.ru'
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'ksercs0@gmail.com'
-EMAIL_HOST_PASSWORD = '181ASDcxz818'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 
 
